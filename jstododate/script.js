7602dd647b3c64bd66b4f7c6635f12dc1b3d3e62
Erdinc <127399545+ErdincOzdemirr@users.noreply.github.com>
2023-08-16 09:55:19 +0300

    Add files via upload

---
+++ jstododate/script.js
@@ -0,0 +1,99 @@
+// @author : Erdinç Özdemir
+//querySelector Used to target the elements in the DOM
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+function runTheClock() {
+  // if we want to use this animation  /* transition:transform .5s ease-in-out; */
+
+  //solving return to zero problem
+
+  // secondPosition=secondPosition+6;
+  // minutePosition=minutePosition+(1/60)*6;
+  // hourPosition=hourPosition+(30/3600);
+
+  var date = new Date();
+  console.log(date);
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("Hour:" + hr + " minute" + min + " second:" + sec);
+
+  let hourPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
+  let minutePosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
+  let secondPosition = (sec * 360) / 60;
+
+  HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
+
+  MINUTEHAND.style.transform = "rotate(" + minutePosition + "deg)";
+
+  SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)";
+}
+// Set Interval method
+// rerun in ever 1000 milisecond i.e 1 sec
+// reference :https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+
+var interval = setInterval(runTheClock, 1000);
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  //setTimeout(showTime,1000) Also can be used instead of setInterval
+
+  let year = date.getFullYear(),
+    month = date.getMonth(),
+    day = String(date.getDate()).padStart(2, "0"),
+    dayNumber = date.getDay();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let humanReadableDate = `${day} ${months[month]} ${year}, ${days[dayNumber]}`;
+  document.getElementById("WatchDay").innerText = humanReadableDate;
+}
+showTime();
+setInterval(showTime, 1000);
+
+document.getElementById("WatchName").innerHTML = "ERDİNÇ ÖZDEMİR";
+let firstName1 = prompt("Lütfen Adınızı Giriniz!");
+document.getElementById("Yazı").innerHTML = ` Hoşgeldin ${firstName1}, <br>
+HAYATTA 1 SN BİLE ÇOK DEĞERLİ !!! `;
